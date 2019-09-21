aa1c4ad81d00db030771aca3e0d70aa1bd70799c
williamdiaz798 <49824200+williamdiaz798@users.noreply.github.com>
2019-09-20 22:49:21 -0600

    proyecto principal
    este proyecto es unicamente para la raiz

---
+++ app/src/main/java/com/example/buscomida_app_electiva_android/Menu_Principal_menu.java
@@ -0,0 +1,14 @@
+package com.example.buscomida_app_electiva_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Principal_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__principal_menu);
+    }
+}
