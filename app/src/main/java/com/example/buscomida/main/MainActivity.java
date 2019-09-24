458bf344c021cd96cb88510bb78481aee54aad2d
williamdiaz798 <49824200+williamdiaz798@users.noreply.github.com>
2019-09-23 22:09:11 -0600

    Union de plantillas
    Se unieron plantillas de cada integrante

---
+++ app/src/main/java/com/example/buscomida/main/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.buscomida.main;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.buscomida.R;
+import com.example.buscomida.category.CategoryFragment;
+import com.example.buscomida.near.NearFragment;
+import com.example.buscomida.preference.PreferenceFragment;
+import com.example.buscomida.restaurant.RestaurantFragment;
+import com.example.buscomida.search.SearchFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        navigationView.setItemTextColor(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(), R.color.text_white, null)));
+        navigationView.setItemIconTintList(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null)));
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.navigation_search:
+                        SearchFragment search = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, search).commit();
+                        Toast.makeText(getApplicationContext(),"Buscar", Toast.LENGTH_LONG).show();
+
+                        break;
+
+                    case R.id.navigation_restaurants:
+                        RestaurantFragment restaurant = new RestaurantFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, restaurant).commit();
+                        Toast.makeText(getApplicationContext(),"Restaurantes", Toast.LENGTH_LONG).show();
+
+                        break;
+
+                    case R.id.navigation_near:
+                        NearFragment container = new NearFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, container).commit();
+                        Toast.makeText(getApplicationContext(),"Cerca de mi", Toast.LENGTH_LONG).show();
+
+                        break;
+
+                    case R.id.navigation_categories:
+                        CategoryFragment category = new CategoryFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, category).commit();
+                        Toast.makeText(getApplicationContext(),"Categorias", Toast.LENGTH_LONG).show();
+
+                        break;
+
+                    case R.id.navigation_preferences:
+                        PreferenceFragment preference = new PreferenceFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, preference).commit();
+                        Toast.makeText(getApplicationContext(),"Preferencias", Toast.LENGTH_LONG).show();
+
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+}
