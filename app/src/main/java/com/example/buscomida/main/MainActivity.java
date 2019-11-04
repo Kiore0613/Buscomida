da1e078f19cebd12150b5b6405cf6bd697ce6b3b
Kiore <ricardochicas95@gmail.com>
2019-11-03 18:02:32 -0600

    Add BottomNavigateView

--- app/src/main/java/com/example/buscomida/main/MainActivity.java
+++ app/src/main/java/com/example/buscomida/main/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.buscomida.main;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.fragment.app.Fragment;
 import com.example.buscomida.BaseAppCompat;
 import com.example.buscomida.R;
 import com.example.buscomida.SharedPref;
@@ -20,14 +16,13 @@ import com.example.buscomida.category.CategoryFragment;
 import com.example.buscomida.near.NearFragment;
 import com.example.buscomida.preference.PreferenceActivity;
 import com.example.buscomida.search.SearchFragment;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends BaseAppCompat {
 
-    NavigationView navigationView;
-    DrawerLayout drawerLayout;
     Toolbar toolbar;
     SharedPref sharedPref;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,59 +32,54 @@ public class MainActivity extends BaseAppCompat {
 
         sharedPref = new SharedPref(this);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        navigationView.setItemTextColor(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(), R.color.colorWhite, null)));
-        navigationView.setItemIconTintList(ColorStateList.valueOf(ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null)));
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                drawerLayout.closeDrawers();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
 
-                switch (menuItem.getItemId()) {
-
-                    case R.id.navigation_search:
-                        SearchFragment search = new SearchFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, search).commit();
-                        setTitle(getResources().getString(R.string.search));
-                        break;
-
-                    case R.id.navigation_near:
-                        NearFragment container = new NearFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, container).commit();
-                        setTitle(getResources().getString(R.string.near));
-                        break;
-
-                    case R.id.navigation_exit:
-
-                        sharedPref.setKeepMeLoggedIn(false);
-                        finish();
-                        break;
-
-                    case R.id.navigation_categories:
-                        CategoryFragment category = new CategoryFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, category).commit();
-                        setTitle(getResources().getString(R.string.categories));
-                        break;
-
-
-                    case R.id.navigation_about:
-                        AboutFragment about = new AboutFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, about).commit();
-                        setTitle(getResources().getString(R.string.about));
-                }
-                return true;
-            }
-        });
-
-        CategoryFragment category = new CategoryFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, category).commit();
-        setTitle(getResources().getString(R.string.categories));
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_layout,
+                    new CategoryFragment()).commit();
+        }
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+
+                        case R.id.navigation_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+
+                        case R.id.navigation_near:
+                            selectedFragment = new NearFragment();
+                            break;
+
+                        case R.id.navigation_categories:
+                            selectedFragment = new CategoryFragment();
+
+                            break;
+
+
+                        case R.id.navigation_about:
+                            selectedFragment = new AboutFragment();
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_layout,
+                            selectedFragment).commit();
+                    return true;
+                }
+
+
+            };
+
 
     @Override
     public void onBackPressed() {
@@ -107,12 +97,21 @@ public class MainActivity extends BaseAppCompat {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.toolbar_preferences) {
-            Intent intent = new Intent(this, PreferenceActivity.class);
-            startActivity(intent);
-            return true;
+        switch (item.getItemId()) {
+            case R.id.toolbar_preferences:
+                Intent intent = new Intent(this, PreferenceActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.toolbar_exit:
+                sharedPref.setKeepMeLoggedIn(false);
+                finish();
+                break;
+
         }
+
         return super.onOptionsItemSelected(item);
+
 
     }
 }
